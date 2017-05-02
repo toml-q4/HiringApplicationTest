e23cb02ac9a7603de1fa665984d4c7a95319cfa1
Daniel Basilio <daniel.basilio90@gmail.com>
2017-05-02 14:05:25 -0500

    changing the razor view to make file upload work right

--- Q4CsvParser.Web/Models/FileUploadModel.cs
+++ Q4CsvParser.Web/Models/FileUploadModel.cs
@@ -1,10 +1,13 @@
-﻿using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
 
 namespace Q4CsvParser.Web.Models
 {
     public class FileUploadModel
     {
+        [Display(Name = "Please select to upload a CSV file")]
         public HttpPostedFileBase File { get; set; }
+        [Display(Name = "Does the file contain a header?")]
         public bool ContainsHeader { get; set; }
     }
 }
